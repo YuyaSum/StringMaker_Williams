9ce10d14e5111501ccc7d631359780bcf0f89cc0
richard williams <richard.williams@student.centralia.edu>
2021-11-30 14:34:42 -0800

    Add project files.

---
+++ StringMaker_Williams/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Dean Williams
+// IT113
+// NOTES: I didn't use any stack or queue as a base to accomplish much, its been a while since I actually had to code and i've
+// had my focuse more so on the 400 class.
+// BEHAVIORS NOT IMPLIMENTED AND WHY: 
+
+namespace StringMaker_Williams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager sm = new StringManager();
+            char choice = 'x';
+            string word = null;
+            string modWord = null;
+
+            while (choice != 'Q')
+            {
+                Console.WriteLine("What would you like to do?\n(M)odify word (N)ew word (Q)uit");
+                choice = Console.ReadKey(true).KeyChar;
+                Console.Clear();
+
+                switch (char.ToUpper(choice)) {
+                    case 'M':
+                        {
+                            if (word == null) {
+                                Console.WriteLine("Try adding a new word first!");
+                                break;
+                            }
+
+                            Console.WriteLine("What would you like to do?\n(A)SCII / (E)quals " +
+                                "/ (R)everse / reverse and (P)reserve case location / s(Y)mmetric / (S)top");
+                            choice = Console.ReadKey(true).KeyChar;
+                            Console.Clear();
+
+                            switch (char.ToUpper(choice)) {
+                                case 'A':
+                                    {
+                                        modWord = sm.ToString();
+                                        sm.ModWord = modWord;
+                                        Console.WriteLine(modWord);
+                                        Console.WriteLine();
+                                        break;
+                                    }
+                                case 'E':
+                                    {
+                                        Console.WriteLine("What word are we checking?");
+                                        var newWord = Console.ReadLine();
+                                        if (sm.Equals(newWord))
+                                        {
+                                            Console.WriteLine("\"" + newWord + "\" is equal to \"" + word + "\"!");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("\"" + newWord + "\" is not equal to \"" + word + "\".");
+                                        }
+                                        Console.WriteLine();
+                                        break;
+                                    }
+                                case 'R':
+                                    {
+                                        modWord = sm.Reverse(modWord);
+                                        sm.ModWord = modWord;
+                                        Console.WriteLine(modWord);
+                                        Console.WriteLine();
+                                        break;
+                                    }
+                                case 'P':
+                                    {
+                                        modWord = sm.Reverse(modWord, true);
+                                        sm.ModWord = modWord;
+                                        Console.WriteLine(modWord);
+                                        Console.WriteLine();
+                                        break;
+                                    }
+                                case 'Y':
+                                    {
+                                        if (sm.Symmetric(modWord))
+                                        {
+                                            Console.WriteLine("\"" + modWord + "\" is Symmetric!");
+                                        }
+                                        else {
+                                            Console.WriteLine("\"" + modWord + "\" is not Symmetric.");
+                                        }
+                                        Console.WriteLine();
+                                        break;
+                                    }
+                                case 'S':
+                                    {
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case 'N':
+                        {
+                            Console.WriteLine("What would you like the word to be?");
+                            word = Console.ReadLine();
+                            sm.OGWord = word;
+                            modWord = word;
+                            Console.WriteLine();
+                            Console.WriteLine("\"" + word + "\" has been added!");
+
+                            break;
+                        }
+                    case 'Q':
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Have a good day!");
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("That isn't a value key.");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ StringMaker_Williams/StringManager.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_Williams
+{
+    class StringManager
+    {
+        public string OGWord { get; set; }
+        public string ModWord { get; set; }
+
+        public string Reverse(string s) {
+            string reverse = "";
+
+            for (int i = s.Length-1; i > -1; i-- ) {
+                reverse += s[i];
+            }
+
+            ModWord = reverse;
+
+            return ModWord;
+        }
+        public string Reverse(string s, bool preserve) {
+            string isLower = "";
+            string lowerReverse = "";
+            string reverse = "";
+            List<int> capLocation = new List<int>();
+
+            for (int i = 0; i < s.Length; i++) {
+                if (char.IsUpper(s[i]))
+                {
+                    capLocation.Add(i);
+                    isLower += char.ToLower(s[i]);
+                }
+                else 
+                { 
+                    isLower += s[i];
+                }
+            }
+
+            for (int i = isLower.Length - 1; i > -1; i--)
+            {
+                lowerReverse += isLower[i];
+            }
+
+            for (int i = 0; i < lowerReverse.Length; i++) {
+                for (int j = 0; j < capLocation.Count(); j++) {
+                    if (i == capLocation[j]) {
+                        reverse += char.ToUpper(lowerReverse[i]);
+                    }
+                }
+                if (i != reverse.Length - 1) {
+                    reverse += lowerReverse[i];
+                }
+            }
+
+            ModWord = reverse;
+
+            return ModWord;
+        }
+        public bool Symmetric(string s) {
+
+            bool sameLetter = false;
+            int end = s.Length - 1;
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] != s[end]) {
+                    sameLetter = false;
+                    break;
+                }
+                end--;
+                sameLetter = true;
+
+
+                //for (int j = s.Length - 1; j >s.Length-i; j--)
+                //{
+                //    if (s[i] != s[j]) {
+                //        return false;
+                //    }
+                //    if (i == Math.Floor(s.Length / 2.0) && j == Math.Ceiling(s.Length / 2.0))
+                //    {
+                //        return true;
+                //    }
+                //    else if (i == j) {
+                //        return true;
+                //    }
+                //}
+            }
+
+            return sameLetter;
+        }
+        //Override ToString to return a string representation of the string input summed ASCII values
+        public override string ToString() {
+            string[] numWords = new string[] {"Zero", "One", "Two", "Three", "Four", "Five", 
+                "Six", "Seven", "Eight", "Nine"};
+            string word = this.OGWord;
+            int ASCII = 0;
+            string ASCIIWord = "";
+
+            foreach (char c in word)
+            {
+                ASCII += (int)c;
+            }
+
+            word = ASCII.ToString();
+
+            foreach (char c in word)
+            {
+                for (int i = 0; i < numWords.Length; i++) {
+                    if (c.ToString() == i.ToString()) {
+                        ASCIIWord += numWords[i] + " ";
+                    }
+                }
+            }
+
+            ModWord = ASCIIWord;
+            
+            return ModWord;
+        }
+        
+        //Override Equals() to compare the current data input to the class with a literal or variable string
+        public override bool Equals(object obj)
+        {
+            if (obj is string)
+            {
+                if ((string)obj == OGWord)
+                {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
